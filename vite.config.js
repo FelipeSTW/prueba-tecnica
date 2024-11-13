5c9051b95306a508572738ceae848fb36de7a794
FelipeSTW <feli.ahumadar@duocuc.cl>
2024-11-13 19:45:20 -0300

    feat: commit inicial

---
+++ vite.config.js
@@ -0,0 +1,16 @@
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
+import { fileURLToPath, URL } from 'url';
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [
+    vue(),
+    // Eliminar vueDevTools() temporalmente para aislar el problema
+  ],
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url))
+    },
+  },
+});
